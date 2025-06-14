932f8fbb9a0c2667c65f5dffdb3290df212534b1
oceanh6672@gmail.com <oceanh6672@gmail.com>
2025-06-14 22:39:52 +0600

    div 2

---
+++ problem_solving/codeforces/A_Soldier_and_Bananas.c
@@ -0,0 +1,39 @@
+/* 
+
+fb dollar banana 
+3 17 4
+13 borrow
+
+1*3 + 2*3 + 3*3 + 4*3 = total
+
+
+
+for (i = 1; i <= banana ; i++){
+    total = total + fb*i
+}
+    
+borrow = total - dollar
+
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int fb, dollar, banana;
+    scanf("%d %d %d", &fb, &dollar, &banana);
+
+    int total = 0, borrow = 0;
+
+    for(int i = 1; i <= banana; i++){
+        total = total + fb*i;
+    }
+
+    if(total > dollar){
+        borrow = total - dollar;
+    }
+    
+    printf("%d", borrow);
+
+    return 0;
+}
