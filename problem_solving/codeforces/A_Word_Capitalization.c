0e6fbe531e148066c9a5627ae299414177a96b71
oceanh6672@gmail.com <oceanh6672@gmail.com>
2025-06-17 16:29:20 +0600

    modified

--- problem_solving/codeforces/A_Word_Capitalization.c
+++ problem_solving/codeforces/A_Word_Capitalization.c
@@ -13,13 +13,15 @@ printf(str)
 
 #include <stdio.h>
 
-int main() {
-    char str[20];
+int main()
+{
+    char str[1001];
     scanf("%s", str);
 
- // if(str[0] >= 'a' && str[0] <= 'z') (safer check)  or, 
+    // if(str[0] >= 'a' && str[0] <= 'z') (safer check)  or,
 
-    if(str[0] > 90){
+    if (str[0] > 90)
+    {
         str[0] = str[0] - 32;
     }
 
@@ -28,4 +30,4 @@ int main() {
 }
 
 // Note: Capital letter er ASCII value'r range 65-90.
-//       er ceye boro hole small letter.  
+//       er ceye boro hole small letter.
